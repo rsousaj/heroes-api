6cac809ebfdd2d24155e6ae533a7bc1704b41e1b
rsousaj <rafasousa@gmail.com>
2021-03-10 09:38:48 -0300

    initial commit.

---
+++ src/test/java/br/com/rsousadj/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsousadj.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
