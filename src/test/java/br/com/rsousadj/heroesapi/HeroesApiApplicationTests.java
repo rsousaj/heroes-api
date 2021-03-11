ffea505645592b95551be3e9a21ffcd4368e414b
rsousaj <rafasousa@gmail.com>
2021-03-11 13:36:04 -0300

    refactor: rename main test application file

--- src/test/java/br/com/rsousadj/heroesapi/HeroesApiApplicationTests.java
+++ src/test/java/br/com/rsousadj/heroesapi/HeroesApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HeroesapiApplicationTests {
+class HeroesApiApplicationTests {
 
 	@Test
 	void contextLoads() {
